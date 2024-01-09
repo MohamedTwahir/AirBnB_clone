7330b8ff1fbccfdf40e7a9ba758ae70cfbe19ee4
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-01-09 08:50:56 -0800

    args and kwargs in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,22 @@ class BaseModel:
 
     """Superclass BaseModel that will be inherited with all other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializing the Base Class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs != {} and kwargs is not None:
+            for key in kwargs:
+                if key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Print string representation of an instance"""
