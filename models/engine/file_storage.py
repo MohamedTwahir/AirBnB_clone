b9f314a0ce491a3e8cca6349b1ca1a8d0f38b6a9
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-01-09 09:42:11 -0800

    storing stage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module containing class FileStorage"""
+import datetime
+import os
+import json
+
+
+class FileStorage:
+
+    """
+    Class that serializes instances to a JSON file and deserializes
+    JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        return the dictionary __objects
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        pass
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
+        pass
