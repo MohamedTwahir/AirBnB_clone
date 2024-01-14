3bdc6b28853b70c37eb1d9032a143362d15d999c
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-01-14 12:50:20 -0800

    initialized the state class

--- models/state.py
+++ models/state.py
@@ -7,3 +7,7 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """Class containing the state"""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializing the class"""
+        super().__init__(*args, **kwargs)
