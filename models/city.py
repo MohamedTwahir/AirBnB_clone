923b87fa7e2060b95a37ad5c96720c070ec7fac2
MohamedTwahir <mohamedtwahir977@gmail.com>
2024-01-14 12:41:10 -0800

    update the city class

--- models/city.py
+++ models/city.py
@@ -8,3 +8,7 @@ class City:
     """class representing a city"""
     state_id = ""
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """City Class Initialization"""
+        super().__init__(*args, **kwargs)
